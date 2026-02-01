4c00331be32df95fb371e1438962dea9ae4c6d93
PeggyC30 <peggy_c30@hotmail.com>
2026-02-01 22:28:35 +0800

    first

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/React_HW-04/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/React_HW-05/" : "/",
   plugins: [react()],
 });
